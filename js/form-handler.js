a206a603a988accf8e1578f39255a62c9ab107c4
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2025-01-22 19:07:53 +0000

    Se corrige la direccion del google script, debodo a correccon de error por CORS

--- js/form-handler.js
+++ js/form-handler.js
@@ -2,7 +2,7 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const formData = new FormData(this);
-    fetch('https://script.google.com/macros/s/AKfycbxW18uOCgu67ZhGrAOEehUOhBEXcR7TuJIlux0uhjjPAK8fBCIyE_7HOgj1gC4eRwbfpw/exec', {
+    fetch('https://script.google.com/macros/s/AKfycby1iH5yW9IiQVLzjJAquZpyh9My8vP1QQgFsa8m7nGGMrRLvdq35iDBvR5X7taROiQmGQ/exec', {
         method: "POST",
         body: JSON.stringify({
             nombre: formData.get('nombre'),
@@ -13,14 +13,14 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
             'Content-Type': 'application/json'
         }
     }).then(response => response.json())
-      .then(data=> {
-        if (data.success) {
-            alert('Mensaje enviado, gracias por cotactarse con nosotros, a la brevedad lo contactaremos');
-            document.getElementById('contactForm').reset();
-        } else {
-            alert('Error: ' +data.message);
-        }
+      .then(data => {
+          if (data.success) {
+              alert('Mensaje enviado, gracias por cotactarse con nosotros, a la brevedad lo contactaremos.');
+              document.getElementById('contactForm').reset();
+          } else {
+              alert('Error: ' + data.message);
+          }
       }).catch(error => {
-        alert('Error al enviar el mensaje, por favor intente nuevamente.');
+          alert('Error al enviar el mensaje, por favor intente nuevamente.');
+          console.error("Error de conexión:", error);
       });
-});
